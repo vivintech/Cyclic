3755ba35e25c1474503472ee32f1df7d16a5128e
Vivien Leonhardt <vivienmichelle.leo@gmail.com>
2024-01-03 23:53:25 +0100

    Install prisma db

---
+++ src/server.js
@@ -0,0 +1,35 @@
+// src/index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+//const pusher = require('pusher');
+const { PrismaClient } = require('@prisma/client');
+const cors = require('cors');
+
+const app = express();
+const db = new PrismaClient();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+/* // Pusher configuration
+const pusherConfig = {
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: 'eu',
+  useTLS: true,
+};
+
+const pusherServer = new pusher(pusherConfig);
+//Export db variable
+module.exports = { db, pusherConfig }; */
+
+// Initializing routes by requiring routes.js
+ require('./routes')(app, db);
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
